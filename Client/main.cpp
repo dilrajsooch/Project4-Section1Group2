ad3133e7b27ba60f5e8a0b18cfb4b40c89180624
Jax <jaxdrummond9@gmail.com>
2025-03-17 13:09:53 -0400

    Setup Raylib and Raygui
    Follow this video https://www.youtube.com/watch?v=UiZGTIYld1M
    as well as do `./vcpkg install raygui:x64-windows` after installing raylib.

--- Client/main.cpp
+++ Client/main.cpp
@@ -1,12 +1,37 @@
-#include <QApplication>
-#include "Client.h"
+#include "raylib.h"
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
 
 int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
+    InitWindow(400, 600, "Bronny's Room");
+    SetTargetFPS(60);
 
-    Client client;
-    client.resize(400, 600);
-    client.show();
+    bool showMessageBox = false;
 
-    return app.exec();
+    while (!WindowShouldClose())
+    {
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+
+
+        if (GuiButton( { 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+
+        if (showMessageBox)
+        {
+            int result = GuiMessageBox({ 85, 70, 250, 100 },
+                "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+
+            if (result >= 0) showMessageBox = false;
+        }
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
 }
