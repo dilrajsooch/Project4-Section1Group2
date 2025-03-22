504e2a97bfad626156c5a173963176cb0543b48d
Jax <jaxdrummond9@gmail.com>
2025-03-22 12:22:23 -0400

    Converted QT GUI code to RayGUI
    This is just to have something to go off of.

--- Client/main.cpp
+++ Client/main.cpp
@@ -5,28 +5,53 @@
 
 
 int main(int argc, char *argv[]) {
-    InitWindow(400, 600, "Bronny's Room");
+    InitWindow(800, 450, "Bronny's Room");
     SetTargetFPS(60);
 
+    // Declare variables to be used with elements
     bool showMessageBox = false;
+    bool inputFieldEditMode = false;
+    char inputFieldText[128] = "Enter Command";
+    bool filterTypeDropdownEditMode = false;
+    int filterTypeDropdownActive = 0;
+    bool sendButtonPressed = false;
+    bool exitButtonPressed = false;
 
     while (!WindowShouldClose())
     {
         // Draw
         //----------------------------------------------------------------------------------
         BeginDrawing();
-        ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
+            ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
 
-        if (GuiButton( { 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+            if (filterTypeDropdownEditMode) GuiLock();
 
-        if (showMessageBox)
-        {
-            int result = GuiMessageBox({ 85, 70, 250, 100 },
-                "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+            GuiLabel({ 350, 80, 160, 24 }, "Disconnected");
+            if (GuiTextBox( { 350, 128, 120, 24 }, inputFieldText, 128, inputFieldEditMode)) inputFieldEditMode = !inputFieldEditMode;
+            sendButtonPressed = GuiButton( { 350, 232, 120, 24 }, "Send");
+            exitButtonPressed = GuiButton( { 350, 288, 120, 24 }, "Exit");
+            if (GuiDropdownBox( { 350, 184, 136, 24 }, "Select Filter Type;AUTHOR;TOPIC", & filterTypeDropdownActive, filterTypeDropdownEditMode)) filterTypeDropdownEditMode = !filterTypeDropdownEditMode;
 
-            if (result >= 0) showMessageBox = false;
-        }
+            GuiUnlock();
+
+
+            // Button with response example
+            if (GuiButton({ 24, 24, 120, 30 }, "#191#Show Message"))
+            {
+                showMessageBox = true;
+            }
+
+            if (showMessageBox)
+            {
+                int result = GuiMessageBox({ 85, 70, 250, 100 },
+                    "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+
+                if (result >= 0)
+                {
+                    showMessageBox = false;
+                }
+            }
 
         EndDrawing();
     }
