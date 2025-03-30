cdd86dcae7bf46fbe021e29b981dbdbb54cae6fb
Jax <jaxdrummond9@gmail.com>
2025-03-30 16:12:22 -0400

    Added Login Page

---
+++ Client/LoginPage.h
@@ -0,0 +1,155 @@
+#pragma once
+#include "PageSystem.h"
+#include "Page.h"
+#include <string>
+
+class LoginPage : public Page {
+private:
+    // Login fields
+    Rectangle loginPanel;
+    Rectangle usernameBox;
+    Rectangle passwordBox;
+    Rectangle loginButton;
+    Rectangle registerButton;
+    
+    // Register fields
+    Rectangle registerPanel;
+    Rectangle regUsernameBox;
+    Rectangle regPasswordBox;
+    Rectangle regConfirmPasswordBox;
+    Rectangle submitRegisterButton;
+    Rectangle backToLoginButton;
+    
+    // Input buffers
+    char username[128] = "";
+    char password[128] = "";
+    char regUsername[128] = "";
+    char regPassword[128] = "";
+    char regConfirmPassword[128] = "";
+    
+    // State
+    bool isRegistering = false;
+    bool loginUsernameEditMode = false;
+    bool loginPasswordEditMode = false;
+    bool registerUsernameEditMode = false;
+    bool registerPasswordEditMode = false;
+    bool registerConfirmPasswordEditMode = false;
+
+public:
+    LoginPage(std::string name) : Page(name) {
+        // Initialize login panel
+        loginPanel = { 200, 100, 400, 250 };
+        usernameBox = { 250, 150, 300, 30 };
+        passwordBox = { 250, 200, 300, 30 };
+        loginButton = { 250, 250, 140, 30 };
+        registerButton = { 410, 250, 140, 30 };
+        
+        // Initialize register panel
+        registerPanel = { 200, 100, 400, 300 };
+        regUsernameBox = { 250, 150, 300, 30 };
+        regPasswordBox = { 250, 200, 300, 30 };
+        regConfirmPasswordBox = { 250, 250, 300, 30 };
+        submitRegisterButton = { 250, 310, 140, 30 };
+        backToLoginButton = { 410, 310, 140, 30 };
+    }
+
+    void Update() override {
+        if (!isRegistering) {
+            // Login panel logic
+            if (GuiButton(loginButton, "Login")) {
+                // Handle login
+                std::string usernameStr(username);
+                std::string passwordStr(password);
+                // TODO: Implement login logic
+                std::cout << "Login attempted with: " << usernameStr << std::endl;
+            }
+
+            if (GuiTextBox(usernameBox, username, 128, loginUsernameEditMode))
+            {
+                loginUsernameEditMode = !loginUsernameEditMode;
+            }
+            if (GuiTextBox(passwordBox, password, 128, loginPasswordEditMode))
+            {
+                loginPasswordEditMode = !loginPasswordEditMode;
+            }
+            
+            
+            if (GuiButton(registerButton, "Register")) {
+                isRegistering = true;
+            }
+        } else {
+            // Register panel logic
+            if (GuiButton(submitRegisterButton, "Submit")) {
+                // Handle registration
+                std::string usernameStr(regUsername);
+                std::string passwordStr(regPassword);
+                std::string confirmPasswordStr(regConfirmPassword);
+                
+                if (passwordStr == confirmPasswordStr) {
+                    // TODO: Implement registration logic
+                    std::cout << "Registration attempted with: " << usernameStr << std::endl;
+                } else {
+                    // TODO: Show error message
+                    std::cout << "Passwords do not match!" << std::endl;
+                }
+            }
+
+            if (GuiTextBox(regUsernameBox, regUsername, 128, registerUsernameEditMode))
+            {
+                registerUsernameEditMode = !registerUsernameEditMode;
+            }
+            if (GuiTextBox(regPasswordBox, regPassword, 128, registerPasswordEditMode))
+            {
+                registerPasswordEditMode = !registerPasswordEditMode;
+            }
+            if (GuiTextBox(regConfirmPasswordBox, regConfirmPassword, 128, registerConfirmPasswordEditMode))
+            {
+                registerConfirmPasswordEditMode = !registerConfirmPasswordEditMode;
+            }
+            
+            if (GuiButton(backToLoginButton, "Back to Login")) {
+                isRegistering = false;
+                // Clear registration fields
+                memset(regUsername, 0, sizeof(regUsername));
+                memset(regPassword, 0, sizeof(regPassword));
+                memset(regConfirmPassword, 0, sizeof(regConfirmPassword));
+            }
+        }
+    }
+
+    void Draw() override {
+        if (!isRegistering) {
+            // Draw login panel
+            GuiPanel(loginPanel, "Login");
+            
+            // Draw labels
+            GuiLabel({ usernameBox.x, usernameBox.y - 20, 100, 20 }, "Username:");
+            GuiLabel({ passwordBox.x, passwordBox.y - 20, 100, 20 }, "Password:");
+            
+            // Draw inputs
+            GuiTextBox(usernameBox, username, 128, loginUsernameEditMode);
+            GuiTextBox(passwordBox, password, 128, loginPasswordEditMode);
+            
+            // Draw buttons
+            GuiButton(loginButton, "Login");
+            GuiButton(registerButton, "Register");
+        } else {
+            // Draw register panel
+            GuiPanel(registerPanel, "Register");
+            
+            // Draw labels
+            GuiLabel({ regUsernameBox.x, regUsernameBox.y - 20, 100, 20 }, "Username:");
+            GuiLabel({ regPasswordBox.x, regPasswordBox.y - 20, 100, 20 }, "Password:");
+            GuiLabel({ regConfirmPasswordBox.x, regConfirmPasswordBox.y - 20, 150, 20 }, "Confirm Password:");
+            
+            // Draw inputs
+            GuiTextBox(regUsernameBox, regUsername, 128, registerUsernameEditMode);
+            GuiTextBox(regPasswordBox, regPassword, 128, registerPasswordEditMode);
+            GuiTextBox(regConfirmPasswordBox, regConfirmPassword, 128, registerConfirmPasswordEditMode);
+            
+            // Draw buttons
+            GuiButton(submitRegisterButton, "Submit");
+            GuiButton(backToLoginButton, "Back to Login");
+        }
+    }
+}; 
--- Client/main.cpp
+++ Client/main.cpp
@@ -2,67 +2,45 @@
 #include "string"
 #include "iostream"
 #include "Packet.h"
+#include "PageSystem.h"
+#include "RoomPage.h"
+#include "LoginPage.h"
 
 #define RAYGUI_IMPLEMENTATION
 #include "raygui.h"
 
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int main() {
     InitWindow(800, 450, "Bronny's Room");
     SetTargetFPS(60);
 
-    // Declare variables to be used with elements
-    Rectangle roomListPanelScrollView = { 0, 0, 0, 0 };
-    Vector2 roomListPanelScrollOffset = { 0, 0 };
-    Vector2 roomListPanelBoundsOffset = { 0, 0 };
-    Rectangle roomChatPanelScrollView = { 0, 0, 0, 0 };
-    Vector2 roomChatPanelScrollOffset = { 0, 0 };
-    Vector2 roomChatPanelBoundsOffset = { 0, 0 };
-    string roomTitle = "Room Title goes here.";
-
-    //------------------------ Testing Packet and Image stuff
-    Image image = LoadImage("2xPoints.png");
-
-    Packet newPacket;
-
-    newPacket.SetBody("Hello World", 12, image);
-
-    cout << "Working Path: " << GetWorkingDirectory() << endl;
-
-    cout << newPacket.GetText() << endl;
-
-    Packet testPacket(newPacket.SerializeData());
-
-    cout << testPacket.GetText() << endl;
-
-    image = testPacket.GetImage();
-
-    ImageResize(&image, 256, 256);
-    Texture2D texture = LoadTextureFromImage(image);
-
-    UnloadImage(image);
-    // ------------------------------- End of Testing Stuff
+    // Create page system
+    PageSystem pageSystem;
+    
+    // Create pages
+    LoginPage* loginPage = new LoginPage("Login");
+    RoomPage* roomPage = new RoomPage("Main Room");
+    
+    // Add pages to the system
+    pageSystem.AddPage(loginPage);
+    pageSystem.AddPage(roomPage);
+    
+    // Start with the login page
+    pageSystem.SwitchToPage("Login");
     
     while (!WindowShouldClose())
     {
+        // Update
+        pageSystem.Update();
+        
         // Draw
-        //----------------------------------------------------------------------------------
         BeginDrawing();
             ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
-
-            DrawTextureRec(texture, {0, 0, 256, 256}, {0, 0}, WHITE);
-            GuiScrollPanel({ 0, 0, 104 - roomListPanelBoundsOffset.x, 728 - roomListPanelBoundsOffset.y }, NULL, { 0, 40, 104, 728 }, &roomListPanelScrollOffset, &roomListPanelScrollView);
-            GuiScrollPanel({ 104, 96, 696 - roomChatPanelBoundsOffset.x, 672 - roomChatPanelBoundsOffset.y }, NULL,  { 104, 96, 992, 672 }, &roomChatPanelScrollOffset, &roomChatPanelScrollView);
-            GuiLabel({ 370, 35, 120, 24 }, roomTitle.c_str());
-
-
+            pageSystem.Draw();
         EndDrawing();
     }
 
-    UnloadTexture(texture);
-
     CloseWindow();
-
     return 0;
 }
