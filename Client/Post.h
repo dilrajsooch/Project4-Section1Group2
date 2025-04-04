69ba5d9b7458c2fc817ca1208ceed0f5b634adc7
Jax <jaxdrummond9@gmail.com>
2025-04-04 15:37:07 -0400

    Add Post class

---
+++ Client/Post.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "string"
+#include <raygui.h>
+
+
+using namespace std;
+
+
+class Post
+{
+private:
+	int roomNumber;
+	string text;
+	Image image;
+	string author;
+
+public:
+	Post(int roomNumber, string text, Image image, string author)
+	{
+		this->roomNumber = roomNumber;
+		this->text = text;
+		this->image = image;
+		this->author = author;
+	}
+
+	int GetRoomNumber()
+	{
+		return roomNumber;
+	}
+
+	string GetText()
+	{
+		return text;
+	}
+
+	Image GetImage()
+	{
+		return image;
+	}
+
+	string GetAuthor()
+	{
+		return author;
+	}
+};
