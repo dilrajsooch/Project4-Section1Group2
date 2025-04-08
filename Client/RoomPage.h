67a40c6b8ee0d828e3a1cd13eed6df979c061548
Jax <jaxdrummond9@gmail.com>
2025-04-08 10:49:00 -0400

    Added function to load room buttons

--- Client/RoomPage.h
+++ Client/RoomPage.h
@@ -2,21 +2,39 @@
 #include "PageSystem.h"
 #include "Packet.h"
 #include "Page.h"
+#include "ChatRoom.h"
+#include "vector"
+#include "CustomElements.h"
 
 class RoomPage : public Page {
 private:
     Rectangle roomListPanelScrollView;
     Vector2 roomListPanelScrollOffset;
-    Vector2 roomListPanelBoundsOffset;
+    float roomListPanelYOffset = 510;
     Rectangle roomChatPanelScrollView;
     Vector2 roomChatPanelScrollOffset;
-    Vector2 roomChatPanelBoundsOffset;
-    std::string roomTitle;
+    vector<ChatRoom> chatRooms;
+    ChatRoom selectedRoom;
 
 
 public: 
-    RoomPage(std::string name) : Page(name) {
-        roomTitle = "Room Title goes here.";
+    RoomPage(std::string name) : Page(name), selectedRoom("Empty"), roomListPanelScrollOffset({ 0,0 }), roomChatPanelScrollOffset({ 0,0 }) {
+        GetRooms();
+    }
+
+    void GetRooms()
+    {
+        // Test code
+        ChatRoom newChatroom("Test");
+        chatRooms.push_back(newChatroom);
+
+        newChatroom.SetName("Test2");
+
+        chatRooms.push_back(newChatroom);
+
+            newChatroom.SetName("Test2");
+        
+        // Use socket here.
     }
 
 
@@ -26,15 +44,45 @@ public:
 
     void Draw() override {
         // Draw your room interface
-        GuiScrollPanel({ 0, 0, 104 - roomListPanelBoundsOffset.x, 728 - roomListPanelBoundsOffset.y }, 
-                      NULL, { 0, 40, 104, 728 }, &roomListPanelScrollOffset, &roomListPanelScrollView);
-        GuiScrollPanel({ 104, 96, 696 - roomChatPanelBoundsOffset.x, 672 - roomChatPanelBoundsOffset.y }, 
-                      NULL, { 104, 96, 992, 672 }, &roomChatPanelScrollOffset, &roomChatPanelScrollView);
-        GuiLabel({ 370, 35, 120, 24 }, roomTitle.c_str());
+        
+        GuiScrollPanel({ 0, 0, 100 , 470  }, 
+            "Rooms", 
+            { 0, 0, 100, roomListPanelYOffset }, 
+            &roomListPanelScrollOffset, 
+            &roomListPanelScrollView);
+        DrawRoomButtons();
+        GuiScrollPanel({ 100, 100, 700, 460}, 
+            "ChatRoom", 
+            {0, 0, 700, 672}, 
+            &roomChatPanelScrollOffset, 
+            &roomChatPanelScrollView);
+        GuiLabel({ 370, 35, 120, 24 }, selectedRoom.GetName().c_str());
+        
     }
 
-    void SetRoomTitle(const std::string& title) {
-        roomTitle = title;
+    void DrawRoomButtons()
+    {
+        int buttonRadius = 32;
+        int buttonSpacing = 5;
+
+        if (roomListPanelYOffset + chatRooms.size() * buttonRadius * 2 > roomListPanelYOffset)
+        {
+            roomListPanelYOffset = chatRooms.size() * (buttonRadius * 2 + buttonSpacing);
+        }
+
+        for (int i = 0; i < chatRooms.size(); i++)
+        {
+           
+
+            ChatRoom chatRoom = chatRooms.at(i);
+            float y = roomListPanelScrollView.y + 40 + i * (buttonRadius * 2 + buttonSpacing) + roomListPanelScrollOffset.y;
+
+            if (GuiCircleButton({ roomListPanelScrollView.x + 40, y }, buttonRadius, chatRoom.GetName().c_str()))
+            {
+                selectedRoom = chatRoom;
+            }
+
+        }
     }
 
 }; 
