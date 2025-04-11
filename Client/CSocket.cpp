276945870ce76696696cf01664e15eda7f2af958
dilrajsooch <65379380+dilrajsooch@users.noreply.github.com>
2025-04-11 16:50:58 -0400

    Update CSocket.cpp
    integrated logger

--- Client/CSocket.cpp
+++ Client/CSocket.cpp
@@ -1,4 +1,22 @@
 #include "CSocket.h"
+#include "Logging.h"
 
 
 CSocket* CSocket::_instance = nullptr;
+
+
+
+Packet CSocket::SendPacket(Packet packet)
+{
+    Logger::getInstance().LogPacket(OUTGOING_PACKET, packet);
+    ...
+    return RecievePacket();
+}
+
+Packet CSocket::RecievePacket()
+{
+    ...
+    Packet newPacket(buffer);
+    Logger::getInstance().LogPacket(INCOMING_PACKET, newPacket);
+    return newPacket;
+}
